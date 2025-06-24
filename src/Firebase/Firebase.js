b1611bb13978fa388c19d1e72c9c63c69fe0e1ac
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-06-25 02:08:24 +0530

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,13 +6,13 @@ import "firebase/storage";
 const provider = new firebase.auth.GoogleAuthProvider();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWpLwq9uhBCpG9cepLzoRJYp_LoHN5iWY",
-  authDomain: "chatify-20eeb.firebaseapp.com",
-  projectId: "chatify-20eeb",
-  storageBucket: "chatify-20eeb.firebasestorage.app",
-  messagingSenderId: "268176489523",
-  appId: "1:268176489523:web:7cac75d95046c5aeb9b746",
-  measurementId: "G-YJ78WG26E0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
